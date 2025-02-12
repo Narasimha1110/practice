84cc8a769198c5c3c1a56e1bad82c1b054357beb
narasimha1110 <narasimhamvnr@gmail.com>
2025-02-11 18:25:39 -0600

    Adding the changes to feature-a

--- feature-a.js
+++ feature-a.js
@@ -1,3 +1,4 @@
 var a=10;
 console.log(a); // 10
-console.log(b); // ReferenceError: b is not defined
+console.log(45)// ReferenceError: b is not defined
+console.log("trying tom push the changes   ")// ReferenceError: b is not defined;
